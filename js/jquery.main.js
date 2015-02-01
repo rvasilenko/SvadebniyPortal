0ffa55d07f9a76d0ffff8b6349a15bab83402f87
vasilenko2000 <ks2974070>
2015-02-01 19:47:43 +0200

    Tabs for social comments

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -28,6 +28,12 @@ function initTabs() {
         tabLinks: 'a.tab-link',
         checkHash: true
     });
+
+    /* comments tabs */
+    $('ul.tabs').delegate('li:not(.current)', 'click', function() {
+        $(this).addClass('current').siblings().removeClass('current')
+            .parents('div.section').find('div.box').hide().eq($(this).index()).fadeIn(150);
+    });
 }
 
 // open-close init
